c5f7be0037fd6c1e0ed2de000c32baeb63bfc6df
zwest1230 <155113607+zwest1230@users.noreply.github.com>
2024-04-25 19:39:22 -0400

    Update app.py
    Updated app.py code.

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+bills = []
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        bill_name = request.form['name']
+        amount_due = request.form['amount']
+        payment_date = request.form['date']
+        payment_amount = request.form['payment']
+        bills.append({'name': bill_name, 'amount': amount_due, 'date': payment_date, 'payment': payment_amount})
+        return redirect(url_for('home'))
+    return render_template('index.html', bills=bills)
 
-@app.route('/calendar')
-def calendar():
-    return render_template('calendar.html')
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
